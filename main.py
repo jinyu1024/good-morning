634b056d2e366605eedcb4d8b30adb46b2d6597c
jinyu1024 <111185861+jinyu1024@users.noreply.github.com>
2022-08-13 23:48:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def get_weather():
 
 
 def get_birthday():
-  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y/%m/%d")
+  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
   if next < datetime.now():
     next = next.replace(year=next.year + 1)
   return (next - today).days
