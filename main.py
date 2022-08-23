62e68f6be2f0cc6778194b5bc2ec71a5104b4b4b
jinyu1024 <111185861+jinyu1024@users.noreply.github.com>
2022-08-23 09:57:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def get_weather():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
   res = requests.get(url).json()
   weather = res['data']['list'][0]
-  return weather['weather'], weather['temp']，weather['low']，weather['high']，weather['date']
+  return weather['weather'], weather['temp'], weather['low'], weather['high'], weather['date'].date()
 
 
 
