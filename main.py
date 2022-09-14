f44b6629d8fa1766239866263a8d118dad14835f
jinyu1024 <111185861+jinyu1024@users.noreply.github.com>
2022-09-14 23:08:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ birthday = os.environ['KAOYAN']
 app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
 
-user1_id = os.environ["USER1_ID"]
+user1_id = os.environ["USER_ID"]
 template_id = os.environ["TEMPLATE_ID"]
 
 
